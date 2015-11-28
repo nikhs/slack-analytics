f5ebff207e4de32ff03c81c5f47654634c635edd
Nikhil Jayasimhan <nikhs.j@gmail.com>
2015-11-29 02:28:38 +0530

    Metadata update

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,14 @@
+ 
+credentials = {
 
-slack_bearer_token = "...Your_slack_token..."
-db = "your_sqlite3_db_.db"
-output_file = "output.html"
+ "client_id"		:	"Your_client_id",
+
+ "client_secret"	:	"Your_client_secret",
+
+ "redirect_uri"		:	"Your_redirect_url",
+
+}
+
+scopes = "channels:read stars:read team:read files:read users:read"
+
+app_secret = "Random_string_to_be_used_as_flask_app_secret"
